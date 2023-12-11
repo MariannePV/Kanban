16cbc178ebc627b37a5f5222dbcefcb794ec2349
nil <nvinyes.smx@institutcampalans.net>
2023-12-11 13:01:56 +0100

    Disseny base

--- Kanban/MainWindow.xaml.cs
+++ Kanban/MainWindow.xaml.cs
@@ -24,5 +24,29 @@ namespace Kanban
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogGrid.Visibility = Visibility.Visible;
+        }
+
+        private void Aceptar_Click(object sender, RoutedEventArgs e)
+        {
+            // Ocultar el cuadro de diálogo después de hacer algo con los datos
+            DialogGrid.Visibility = Visibility.Collapsed;
+
+            // Obtiene los datos desde el cuadro de diálogo
+            string nombre = txtNombre.Text;
+            string descripcion = txtDescripcion.Text;
+
+            // Lógica para agregar el Post-it en la zona correspondiente
+            AgregarPostItALaZona(nombre, descripcion);
+        }
+        private void AgregarPostItALaZona(string nombre, string descripcion)
+        {
+            // Lógica para agregar el Post-it en la zona correspondiente
+            // ...
+        }
+
     }
 }
